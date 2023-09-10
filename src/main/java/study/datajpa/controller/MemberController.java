adf960d96e38fbb3ddac5fdae99acbecb37750a3
siyeon <itsiyeon@nate.com>
2023-09-10 23:15:00 +0900

    :zap: Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,9 +2,14 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -24,8 +29,18 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size=5, sort="username") Pageable pageable) {
+        PageRequest request = PageRequest.of(1, 2);
+        Page<Member> page = memberRepository.findAll(request);
+        //MyPage<MemberDto> 원하는 스타일로 바꿔도 돼
+        return page.map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for(int i=0;i<100;i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
